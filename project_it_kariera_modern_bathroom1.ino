e892c794cde999a43ea16c0ae988a7832f4a0820
Ismetmetin <107638539+Ismetmetin@users.noreply.github.com>
2023-05-08 12:53:07 +0300

    Add files via upload

---
+++ project_it_kariera_modern_bathroom1.ino
@@ -0,0 +1,51 @@
+int infraredSensor=3;
+int tissueDispenser = 11;
+int dcFan = 10;
+int slideSwitch= 13;
+int slideSwitchOn=1;
+int lightbulb = 9;
+
+
+void setup()
+{
+  pinMode(infraredSensor, INPUT);
+  pinMode(tissueDispenser, OUTPUT);
+  pinMode(dcFan, OUTPUT);  
+  pinMode(slideSwitch,INPUT);
+  pinMode(lightbulb, OUTPUT);
+}
+
+void loop()
+{ 
+	slideSwitchFunc();  
+  
+  	paperDispenser();
+}
+
+
+void slideSwitchFunc(){
+  
+int read= digitalRead(slideSwitch);
+  if(read==1){
+    analogWrite(dcFan,1020);
+    analogWrite(lightbulb,1020);
+  }else
+  {
+  	analogWrite(dcFan,0);
+    analogWrite(lightbulb,0);
+  }
+}
+
+
+void paperDispenser(){
+if(digitalRead(infraredSensor)){
+   analogWrite(tissueDispenser,20);
+   delay(1500);
+   analogWrite(tissueDispenser,0);
+   delay(3500);//delay mejdu sledvashtoto puskane na hartiqta
+  }else{
+  analogWrite(tissueDispenser,0);
+  }
+
+
+}
